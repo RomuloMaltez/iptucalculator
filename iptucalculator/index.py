fd7a42963bba6f8cd805598713b332edf29ea55f
RomuloMaltez <MALTEZROMULO@GMAIL.COM>
2025-05-12 12:25:57 -0400

    Add files via upload

---
+++ iptucalculator/index.py
@@ -0,0 +1,6 @@
+from app import app
+
+# Este arquivo serve como ponto de entrada para o Vercel
+# O Vercel procura automaticamente por um arquivo chamado index.py
+
+# Não precisamos executar app.run() aqui, pois o Vercel cuidará disso
